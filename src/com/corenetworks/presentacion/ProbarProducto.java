d50f53c0919d04967b7784c23447569d7e363f03
Cristina <cristinadel13@hotmail.com>
2023-11-06 19:03:41 +0100

    Herencias

--- src/com/corenetworks/presentacion/ProbarProducto.java
+++ src/com/corenetworks/presentacion/ProbarProducto.java
@@ -1,2 +1,15 @@
-package com.corenetworks.presentacion;public class ProbarProducto {
+package com.corenetworks.presentacion;
+
+import com.corenetworks.modelo.Producto;
+
+public class ProbarProducto {
+    public static void main(String[] args) {
+        ProductoPerecedero pp1 = new ProductoPerecedero("leche",2, 1.2,"2023/12/31");
+        System.out.println(pp1.toString());
+
+        Producto p1 = new Producto("pan",1,0.8);
+        Producto p2 = new Producto("pan",1,0.8);
+        System.out.println(p1.toString());
+        System.out.println(p1.equals("resultado del metodo equals es ->"+ p1.equals(p2)));
+    }
 }
--- src/com/corenetworks/presentacion/ProductoPerecedero.java
+++ src/com/corenetworks/presentacion/ProductoPerecedero.java
@@ -1,2 +1,49 @@
-package com.corenetworks.presentacion;public class ProductoPerecedero {
+package com.corenetworks.presentacion;
+
+import com.corenetworks.modelo.Producto;
+
+public class ProductoPerecedero extends Producto {
+
+    private String fCadicidad;
+
+
+
+    public ProductoPerecedero() {
+
+
+
+
+
+
+    }
+
+    public ProductoPerecedero(String nombre, int cantidad, double pvp, String fCadicidad) {
+        super(nombre, cantidad, pvp);
+        this.fCadicidad = fCadicidad;
+
+
+    }
+
+    public String getfCadicidad() {
+        return fCadicidad;
+    }
+
+    public void setfCadicidad(String fCadicidad) {
+        this.fCadicidad = fCadicidad;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductoPerecedero{" +
+                "fCadicidad='" + fCadicidad + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", cantidad=" + cantidad +
+                ", pvp=" + pvp +
+                '}';
+
+
+    }
+
+
 }
+
